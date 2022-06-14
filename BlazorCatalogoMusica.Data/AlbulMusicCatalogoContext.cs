1e035623150d5c7cd775c95a39e6320baf39119e
Enrique Salinas <kikotes8686@gmail.com>
2022-06-14 16:44:10 +0200

    Configurar dbcontext con modelos y cadena de conexion

---
+++ BlazorCatalogoMusica.Data/AlbulMusicCatalogoContext.cs
@@ -0,0 +1,22 @@
+﻿using BlazorCatalogoMusica.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCatalogoMusica.Data
+{
+    // Nexo entre la base de datos y el código
+    public class AlbulMusicCatalogoContext : DbContext
+    {
+        // Modelos para mapear 
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CatalogoMusica;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
