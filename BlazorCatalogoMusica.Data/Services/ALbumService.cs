cf5af67797b6ac4acb7a222d1ef039b16c01336e
Enrique Salinas <kikotes8686@gmail.com>
2022-06-15 16:55:14 +0200

    update albumy y canciones

--- BlazorCatalogoMusica.Data/Services/ALbumService.cs
+++ BlazorCatalogoMusica.Data/Services/ALbumService.cs
@@ -41,9 +41,11 @@ namespace BlazorCatalogoMusica.Data.Services
             return await _context.SaveChangesAsync() > 0;
         }
 
-        public Task<bool> UpdateAlbum(Album album)
+        public async Task<bool> UpdateAlbum(Album album)
         {
-            throw new NotImplementedException();
+            _context.Update(album);
+
+            return await _context.SaveChangesAsync() > 0;
         }
     }
 }
